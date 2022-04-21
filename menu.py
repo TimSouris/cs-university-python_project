64c89a0e47f203f7cb335001e3e4991df5c2c312
TimSouris <isaacs92@protonmail.com>
2022-04-21 08:56:14 +0400

    .

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 # Importation des libraires et autres classes
+from tkinter import CENTER
 import main
 import pygame
 import sys
@@ -10,18 +11,18 @@ pygame.init()
 res = (HEIGHT,WIDTH) 
 screen = pygame.display.set_mode(res)  
 smallfont = pygame.font.SysFont('Corbel',35) 
-text = smallfont.render('Adresse IP locale :' , True , BLUE)
+text = smallfont.render('Adresse IP locale :', True, BLUE)
 while True: 
     for ev in pygame.event.get(): 
         if ev.type == pygame.QUIT: 
-            pygame.quit()   
+            pygame.quit()
         if ev.type == pygame.MOUSEBUTTONDOWN:
-            if WIDTH/2 <= mouse[0] <= WIDTH/2+140 and HEIGHT/2 <= mouse[1] <= HEIGHT/2+40: 
+            if WIDTH/2 <= mouse[0] <= WIDTH/2+140 and HEIGHT/2 <= mouse[1] <= HEIGHT/2+40:
                 pygame.quit()
-    mouse = pygame.mouse.get_pos() 
-    if WIDTH/2 <= mouse[0] <= WIDTH/2+140 and HEIGHT/2 <= mouse[1] <= HEIGHT/2+40: 
-        pygame.draw.rect(screen,BLUE,[WIDTH/2,HEIGHT/2,400,40])     
-    else: 
-        pygame.draw.rect(screen,BLUE,[WIDTH/2,HEIGHT/2,400,40]) 
-    screen.blit(text,(WIDTH/2+50,HEIGHT/2))
-    pygame.display.update() 
+    mouse = pygame.mouse.get_pos()
+    if WIDTH/2 <= mouse[0] <= WIDTH/2+140 and HEIGHT/2 <= mouse[1] <= HEIGHT/2+40:
+        pygame.draw.rect(screen,WHITE,[WIDTH,HEIGHT,400,1000])
+    else:
+        pygame.draw.rect(screen,BLUE,[WIDTH,HEIGHT,400,1000])
+    screen.blit(text,(WIDTH/2-50,HEIGHT/2-50))
+    pygame.display.update()
