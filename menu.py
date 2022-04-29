ef25466ef421079ad17f2adade0207bad791cd8b
TimSouris <isaacs92@protonmail.com>
2022-04-29 15:55:55 +0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from dis import Instruction
 from platform import python_branch
 from turtle import Screen, color
@@ -14,10 +15,22 @@ pygame.init()
 screen=pygame.display.set_mode(WINDOW_SIZE)
 base_font = pygame.font.Font(None, 45)
 base_font2= pygame.font.Font(None, 20)
+base_font3=pygame.font.Font(None,40)
 titre='Jeu de Dames !'
 ordre='Entre ton adresse réseau local pour jouer avec un ami (si tu en as)'
-user_text=''
+
+#Ce qui sera rentré dans le champ de saisie
+user_text='  '
+
+#Le rectangle du champ de saisie
 input_rect = pygame.Rect(150,200,100,32)
+
+#Le rectangle de la touche entrée
+enter_rect=pygame.Rect(175,250,155,40)
+
+#Le texte "ENTRER"
+entree='ENTREZ !'
+
 color=pygame.Color('blue')
 
 while True:
@@ -25,8 +38,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_BACKSPACE:
+                #Pour pouvoir utiliser la touche Effacer
                 user_text=user_text[:-1]
             elif event.key == pygame.K_KP_ENTER:
                 print(user_text)
@@ -34,17 +49,31 @@ while True:
                 user_text+=event.unicode
     
     screen.fill((0,0,0))
+
     #Titre
     text_titre=base_font.render(titre,True,BLUE)
     screen.blit(text_titre,(150,70))
+
     #consigne d'entrer l'IP
     text_ordre=base_font2.render(ordre,True,BLUE)
     screen.blit(text_ordre,(50,150))
+
     #champ de saisie
     pygame.draw.rect(screen,color,input_rect)
+
     text_surface=base_font.render(user_text,True,(255,255,255))
     screen.blit(text_surface,(input_rect.x +5,input_rect.y+5))
+
     #pour redimensionner le champ de saisie
     input_rect.w=max(200,text_surface.get_width()+10)
+
+    #touche Entrée
+    pygame.draw.rect(screen,GREEN,enter_rect)
+
+    #texte entrée
+    text_entree=base_font.render(entree,True,WHITE)
+    screen.blit(text_entree,(enter_rect.x+5,enter_rect.y+5))
     pygame.display.flip()
     CLOCK.tick(60)
+
+    
